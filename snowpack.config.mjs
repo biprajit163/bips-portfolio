8b2b9400060db92ccb9007687ec73ffba1e17989
Biprajit Majumder <biprajit163@gmail.com>
2021-07-21 21:48:38 -0400

    configure react and typescript

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    public: '/',
+    src: '/dist',
   },
   plugins: [
-    ['@snowpack/plugin-webpack'],
+    '@snowpack/plugin-webpack', 
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+interface AppProps {};
+
+function App({}: AppProps) {
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>Biprajit Majumder</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
